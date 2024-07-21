56a04f5e4ace9c439f37ebdb5758b6aeab631ebb
nidhinr1 <151380712+nidhinr1@users.noreply.github.com>
2024-07-21 21:58:05 +0530

    Add files via upload

--- billingsystemapp/models.py
+++ billingsystemapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import uuid
 class Category(models.Model):
     name = models.CharField(max_length=100)
     
@@ -39,13 +40,13 @@ class Sales(models.Model):
     
 class Billing(models.Model):
     id = models.AutoField(primary_key=True)
-    sale_number=models.IntegerField(null=True)
-    product_name = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.IntegerField()
+    sale_number=models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     purchasetime=models.DateTimeField(null=True)
 
     def __str__(self):
-        return str(self.id)
+        return str(self.sale_number)
 
 
